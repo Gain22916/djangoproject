8e81e4830d82f4bfd388468e7c2665cba7e6bbb1
GainHunter <46259112+GainHunter@users.noreply.github.com>
2019-09-28 23:01:42 +0700

    lineAPI successfully 001

--- admin_management/models.py
+++ admin_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+#from admin_management import LineAPI
 
 # Create your models here.
+
+class Intruder(models.Model) : 
+    intru = models.CharField(max_length=20)
+    cam = models.CharField(max_length=10)
+    timedetect = models.DateTimeField('TD', default=timezone.now())
+
+    #def lineintruder(self):
+    #   LineAPI.lineNotify('Detect Intruder')
+         
